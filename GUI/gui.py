95515ca3f1b737bb37605e6014344e726511347e
Demeyer10 <04_zoster_borscht@icloud.com>
2024-06-02 17:23:41 -0400

    Messing around with python GUI

---
+++ GUI/gui.py
@@ -0,0 +1,25 @@
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        self.setFixedSize(QSize(400, 300))
+
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+
+window = MainWindow()
+window.show()
+
+app.exec()
